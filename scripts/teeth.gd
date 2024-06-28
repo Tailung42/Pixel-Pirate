c78827463d4aa52acaeb3d4cd5b4059974811c28
Bipin Thapa <tailung12345678910@gmail.com>
2024-06-28 17:04:53 +0545

    enemy jumps successfully

--- scripts/teeth.gd
+++ scripts/teeth.gd
@@ -2,18 +2,19 @@ extends CharacterBody2D
 
 #constants
 const SPEED = 200.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -420.0
 # variables
 var direction = 1
 var is_running = false
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+var iswaiting = false
 
 func _physics_process(delta):
-	if not is_running and not ($RayCastLeft.is_colliding() or $RayCastRight.is_colliding()):
+
+	if not is_running and is_on_floor():
 		start_running()
 
-	# Add the gravity.
+	# Add the gravity.s
 	if not is_on_floor():
 		velocity.y += gravity * delta
 	else:
@@ -21,13 +22,14 @@ func _physics_process(delta):
 
 	# Handle jump.
 	if $RayCastUp.is_colliding() and is_on_floor():
+		stop_running()
 		velocity.y = JUMP_VELOCITY
+
 
 	# handles wall collision
 	if $RayCastRight.is_colliding() and is_running:
 		stop_running()
 		$RayCastRight.enabled = false
-		# await get_tree().create_timer(1.0).timeout
 		change_direction()
 		$RayCastLeft.enabled = true
 		start_running()
@@ -35,10 +37,23 @@ func _physics_process(delta):
 	if $RayCastLeft.is_colliding() and is_running:
 		stop_running()
 		$RayCastLeft.enabled = false 
-		# await get_tree().create_timer(1.0).timeout 
 		change_direction()
 		$RayCastRight.enabled = true
 		start_running()
+
+		# handle appropriate animation
+	if is_on_floor():
+		if velocity.length() == 0:
+			$AnimatedSprite2D.play("idle")
+		else:   # if velocity is not 0
+			$AnimatedSprite2D.play("run")
+	else:
+		if velocity.y < 0:
+			$AnimatedSprite2D.play("jump")
+		else:
+			$AnimatedSprite2D.play("fall")
+
+
 
 	move_and_slide()
 
@@ -50,7 +65,7 @@ func stop_running():
 
 func change_direction():
 	direction = -direction
-
+	$AnimatedSprite2D.flip_h = direction > 0
 
 func start_running():
 	is_running = true

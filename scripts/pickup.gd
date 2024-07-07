65cf795a5e2602a34aebfa107a93e39b8438214c
Bipin Thapa <tailung12345678910@gmail.com>
2024-07-07 10:56:25 +0545

    removed print

--- scripts/pickup.gd
+++ scripts/pickup.gd
@@ -2,7 +2,6 @@ extends Area2D
 
 func _on_body_entered(body:Node2D):
 	$AudioStreamPlayer.play()
-	print("picked")
 	$AnimatedSprite2D.visible = false
 	await get_tree().create_timer(0.5).timeout
 	queue_free()
